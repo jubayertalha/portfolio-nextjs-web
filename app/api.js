804da8b38e21edb105835985247430a4c4e45b0e
Talha Jubayer <jubayertalha194@gmail.com>
2024-09-01 15:11:41 +0000

    changed port

--- app/api.js
+++ app/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchPosts = async () => {
   try {
-    const response = await axios.get('http://api.trahman.me:1337/api/blogs');
+    const response = await axios.get('http://api.trahman.me/api/blogs');
     return response.data.data;
   } catch (error) {
     console.error('Error fetching posts:', error);
